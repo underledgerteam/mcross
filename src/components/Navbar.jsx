5e3ba948ee9865e7c837d34e10bfcd8b5e37518a
Mob <tammanoon24@gmail.com>
2022-05-09 10:34:10 +0700

    Change useNavigate to Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 // use context store wallet data to render wallet adress
 
 const navData = [
@@ -26,40 +26,26 @@ const navData = [
 ];
 
 const Navbar = () => {
-    const ref = useRef();
-    let navigate = useNavigate();
-
-    const redirectTo = (path) => {
-        navigate(path);
-    }
-
     return (
         <div className="bg-primary">
             <div className="flex container w-full mx-auto justify-between items-center font-bold text-2xl px-5 py-6">
-                <div
-                    className="h-[2rem] flex items-center text-center text-white uppercase cursor-pointer"
-                    alt="logo"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        redirectTo("/");
-                    }}
-                >
-                    mcross
-                </div>
+                <Link to='/'>
+                    <div
+                        className="h-[2rem] flex items-center text-center text-white uppercase cursor-pointer"
+                        alt="logo"
+                    >
+                        mcross
+                    </div>
+                </Link>
 
                 <nav className="hidden md:flex space-x-10 items-center text-white">
                     {navData.map((item) => {
                         return (
-                            <div
-                                key={item.name}
-                                className="text-xl uppercase cursor-pointer"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    redirectTo(item.href);
-                                }}
-                            >
-                                {item.name}
-                            </div>
+                            <Link key={item.name} to={item.href}>
+                                <div className="text-xl uppercase cursor-pointer">
+                                    {item.name}
+                                </div>
+                            </Link>
                         );
                     })}
                 </nav>

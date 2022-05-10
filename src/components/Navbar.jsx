a209dd1c6a020f68c060f60120c1792bb7a6b878
Mob <tammanoon24@gmail.com>
2022-05-10 15:49:36 +0700

    Update layout style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,64 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 // use context store wallet data to render wallet adress
 
 const navData = [
-    {
-        name: "home",
-        href: "/",
-    },
-    {
-        name: "mint nft",
-        href: "/mint",
-    },
-    {
-        name: "marketplace",
-        href: "/market",
-    },
-    {
-        name: "nft converse",
-        href: "/converse",
-    },
-    {
-        name: "profile",
-        href: "/profile",
-    }
+  {
+    name: "home",
+    href: "/",
+  },
+  {
+    name: "mint nft",
+    href: "/mint",
+  },
+  {
+    name: "marketplace",
+    href: "/market",
+  },
+  {
+    name: "nft converse",
+    href: "/converse",
+  },
+  {
+    name: "profile",
+    href: "/profile",
+  }
 ];
 
 const Navbar = () => {
-    return (
-        <div className="bg-primary">
-            <div className="flex container w-full mx-auto justify-between items-center font-bold px-5 py-6">
-                <Link to='/'>
-                    <div
-                        className="h-[2rem] flex text-center text-2xl text-white uppercase cursor-pointer"
-                        alt="logo"
-                    >
-                        mcross
-                    </div>
-                </Link>
+  const { pathname } = useLocation();
+  return (
+    <div className="bg-custom-navbar drop-shadow-navbar">
+      <div className="flex container w-full mx-auto justify-between items-center font-bold px-5 py-6">
+        <Link to='/'>
+          <div
+            className="h-[2rem] flex text-center text-2xl text-white uppercase cursor-pointer"
+            alt="logo"
+          >
+            mcross
+          </div>
+        </Link>
 
-                <nav className="md:flex space-x-10 items-center text-white">
-                    {navData.map((item) => {
-                        return (
-                            <Link key={item.name} to={item.href}>
-                                <div className="text-xl uppercase hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 cursor-pointer">
-                                    {item.name}
-                                </div>
-                            </Link>
-                        );
-                    })}
-                    <button 
-                        type="button" 
-                        className="w-48 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-4 text-xl rounded"
-                    >
-                        Connect Wallet
-                    </button>
-                </nav>
-            </div>
-        </div>
-    )
-}
+        <nav className="md:flex space-x-10 items-center">
+          {navData.map((item) => {
+            const isActive = (pathname === item.href) ? "text-white" : "text-custom-gray1 opacity-70";
+            return (
+              <Link key={item.name} to={item.href}>
+                <div className={`bg-custom-navbar text-xl uppercase ${isActive} hover:opacity-100 hover:text-white active:bg-pink-500 active:opacity-100 cursor-pointer`}>
+                  {item.name}
+                </div>
+              </Link>
+            );
+          })}
+          <button
+            type="button"
+            className="btn-connect"
+          >
+            Connect Wallet
+          </button>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/containers/layout/Layout.jsx
+++ src/containers/layout/Layout.jsx
@@ -3,14 +3,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from '../../components/Navbar';
 
 const Layout = () => {
-    return (
-        <>
-            <Navbar />
-            <div className="layout">
-                <Outlet />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="bg-custom-black text-white">
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default Layout;

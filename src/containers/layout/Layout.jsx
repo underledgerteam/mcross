3074b5d35b77fb3738452f6679c359bdcd022bb1
Mob <tammanoon24@gmail.com>
2022-05-11 11:31:22 +0700

    Add padding

--- src/containers/layout/Layout.jsx
+++ src/containers/layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className="min-h-screen min-w-screen flex flex-col">
       <Navbar />
-      <div className="flex flex-grow text-white">
+      <div className="flex flex-grow text-white p-4 md:p-8">
         <Outlet />
       </div>
     </div>

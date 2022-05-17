2ee271a6ed78a6d3cf56bc3a49206fa4076b4619
Kaseres <arenas.2533@gmail.com>
2022-05-18 02:50:30 +0700

    Trello #5 Mockup Data and create shared marketplace

---
+++ src/components/shared/card/CardDetail.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const CardDetail = ({ name, price, image, rarity, chain, onClick, onClickAction }) => {
+
+  return (
+    <div className="w-full rounded overflow-hidden shadow-md hover:shadow-xl bg-[#292929] relative group">
+      <img className="w-full cursor-pointer" src={image} alt={name} onClick={onClick} />
+      <div className="px-6 py-4">
+        <div className="grid grid-cols-2">
+          <p><span className="bg-slate-100 text-slate-800 text-xs font-semibold px-1.5 py-0.5 rounded">🔗{chain}</span></p>
+          <p className="text-right"><span className="bg-yellow-100 text-yellow-800 text-xs font-semibold px-2.5 py-0.5 rounded">{rarity} #</span></p>
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="font-bold text-white text-xl mb-2 mt-3 cursor-pointer" onClick={onClick}>{name}</div>
+          {price && (<div className="font-bold text-white text-right justify-right text-xl mb-2 mt-3">🪙: {price} ETH</div>)}
+        </div>
+        <div className="mt-5">
+          <button
+            className={`w-full font-bold py-3 px-12 rounded bg-gradient-to-r from-green-500 to-blue-600 text-white hover:from-pink-500 hover:to-yellow-500 z-50`}
+            onClick={onClickAction}
+          >
+            Buy
+          </button>
+        </div>
+
+      </div>
+
+    </div >
+  );
+};
+
+export default CardDetail;
--- src/pages/market/Market.jsx
+++ src/pages/market/Market.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Web3Provider } from "../../contexts/connect.context";
+
+import { NFT_DATA } from "../../assets/data/mockUpData";
+import CardDetail from "../../components/shared/card/CardDetail";
 
 const Market = () => {
-    return (
-        <div>
-            Market
-        </div>
-    )
-}
+
+  const { account, ConnectedWallet } = useContext(Web3Provider);
+
+  const data = NFT_DATA;
+
+  const handleClickName = (id) => {
+    console.log(id);
+  };
+
+  const handleClickAction = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <div className="w-full flex flex-col">
+      <div className="mx-auto font-bold text-4xl text-white uppercase">Marketplace</div>
+      <div className="h-full flex gap-x-4 lg:gap-x-8 justify-center">
+
+        {!account ? (
+          <div className="w-full lg:w-1/2 flex-auto py-8 px-8 text-center">
+            <div className="">
+              <button type="button" className="w-96 px-10 py-4 btn-home" onClick={ConnectedWallet}>
+                Connect Wallet
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="w-full">
+            <div className="py-8 px-8 shadow-lg rounded-lg my-20 backdrop-blur-lg bg-[#323652]/50">
+              <div className="flex flex-col">
+                <div className="grid lg:grid-cols-4 grid-cols-1 gap-4">
+                  {data?.map((item) => (
+                    <div className="w-full rounded overflow-hidden shadow-md hover:shadow-xl bg-[#292929] relative group" key={item.id} >
+                      <CardDetail
+                        id={item.id}
+                        name={item.title}
+                        price={item.currentBid}
+                        image={item.imgUrl}
+                        rarity={'Common'}
+                        chain={'Ethereum'}
+                        onClick={() => handleClickName(item.id)}
+                        onClickAction={() => handleClickAction(item.id)}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+      </div>
+
+    </div >
+  );
+};
 
 export default Market;

208ae0a4a9d7d8196b7fd0c3348ef47e8fe7605a
Mob <tammanoon24@gmail.com>
2022-06-14 15:20:12 +0700

    🚧 T80 add getCrossChainGasPrice

--- src/services/gas.service.js
+++ src/services/gas.service.js
@@ -1,5 +1,7 @@
-const axios = require("axios");
+import Web3 from "web3";
+import { NFT_CONTRACTS, BRIDGE_GAS_LIMIT } from "../utils/constants";
 
+const axios = require("axios");
 const addressZero = "0x0000000000000000000000000000000000000000";
 
 export const getGasPrice = async (
@@ -34,4 +36,15 @@ export const getGasPrice = async (
   const dest = result.destination_native_token;
   const destPrice = 1e18 * dest.gas_price * dest.token_price.usd;
   return destPrice / result.source_token.token_price.usd;
+};
+
+export const getCrossChainGasPrice = (sourceChain, destinationChain) => {
+  // (BRIDGE_GAS_LIMIT * GAS_PRICE[destination_chain]) / 1000000000
+  // (gasDestToken * GAS_TOKEN_PRICE[destination_chain]) / GAS_TOKEN_PRICE[source_chain];
+  const gasDestToken = ((BRIDGE_GAS_LIMIT * NFT_CONTRACTS[destinationChain].GAS_PRICE) / 1e+9); // 1e+9
+  const gasSourceToken = (gasDestToken * NFT_CONTRACTS[destinationChain].GAS_TOKEN_PRICE) / NFT_CONTRACTS[sourceChain].GAS_TOKEN_PRICE;
+  console.log(gasSourceToken);
+  console.log(Web3.utils.toWei(gasSourceToken.toString()));
+  console.log(Web3.utils.toWei(gasSourceToken.toString(), "gwei"));
+  return Web3.utils.toWei(gasSourceToken.toString(), "gwei");
 };
